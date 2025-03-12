4b55962fca15f2f2932bd609f632ad7ae1eb9f36
suffyanansari <ansarisuffyan120@gmail.com>
2025-03-12 06:48:57 +0500

    mynewwork1

--- app.js
+++ app.js
@@ -10,3 +10,19 @@ document.getElementById("hamburger").addEventListener("click", function() {
     marquee.addEventListener("mouseout", function() {
         marquee.start(); // Start marquee when mouse leaves
     });
+    $('#carouselExampleIndicators').on('slide.bs.carousel', function (e) {
+        // Remove all font style classes
+        $('.carousel-item').removeClass('font-style-1 font-style-2 font-style-3');
+    
+        // Add the appropriate class to the active carousel item
+        var activeItem = $(e.relatedTarget);
+        
+        // Check which slide is active and add the corresponding font style class
+        if (activeItem.index() === 0) {
+            $('.carousel-item').eq(0).addClass('font-style-1');
+        } else if (activeItem.index() === 1) {
+            $('.carousel-item').eq(1).addClass('font-style-2');
+        } else if (activeItem.index() === 2) {
+            $('.carousel-item').eq(2).addClass('font-style-3');
+        }
+    });
